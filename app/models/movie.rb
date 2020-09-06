2b622559697bfabcb20393e867b0f5b244561e92
Brant Wellman <brantwellman@gmail.com>
2020-09-05 18:38:04 -0600

    Add movie annotations

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: movies
+#
+#  id           :integer          not null, primary key
+#  poster_path  :string
+#  release_date :date
+#  title        :string
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#  imdb_id      :string
+#  tmdb_id      :string
+#
 class Movie < ApplicationRecord
   SMALL_LOGO = "w45"
   MEDIUM_LOGO = "w154"
--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: movies
+#
+#  id           :integer          not null, primary key
+#  poster_path  :string
+#  release_date :date
+#  title        :string
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#  imdb_id      :string
+#  tmdb_id      :string
+#
 require 'rails_helper'
 
 RSpec.describe Movie, type: :model do
