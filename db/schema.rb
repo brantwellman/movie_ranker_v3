b316eb4315f063b56660a60b98c4ac25ea4fb621
Brant Wellman <brantwellman@gmail.com>
2020-09-06 15:57:47 -0600

    Add user migration and model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_05_224700) do
+ActiveRecord::Schema.define(version: 2020_09_06_213120) do
 
   create_table "movies", force: :cascade do |t|
     t.string "title"
@@ -20,6 +20,19 @@ ActiveRecord::Schema.define(version: 2020_09_05_224700) do
     t.string "tmdb_id"
     t.string "imdb_id"
     t.date "release_date"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.string "username"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
