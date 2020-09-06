6e0fe64733f13217ce7f7f5c1a49361cbb26c1bf
Brant Wellman <brantwellman@gmail.com>
2020-09-05 18:22:13 -0600

    Add movie save and show functionality

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,20 @@
 class MoviesController < ApplicationController
   def create
-    # binding.pry
+    tmdb_id = params[:id]
+    movie = Tmdb::Movie.detail(tmdb_id)
+
+    @movie = Movie.find_or_create(movie)
+
+    if @movie.save
+      flash[:success] = "You added another movie to your collection"
+      redirect_to movie_path(@movie)
+    else
+      flash[:error] = "Something went wrong. Please try again"
+      redirect_to search_path
+    end
+  end
+
+  def show
+    @movie = Movie.find(params[:id])
   end
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,26 @@
 class Movie < ApplicationRecord
   SMALL_LOGO = "w45"
+  MEDIUM_LOGO = "w154"
   BASE_URL = "http://image.tmdb.org/t/p/"
+
+  def self.find_or_create(raw_movie)
+    movie_attributes = to_attributes(raw_movie)
+
+    Movie.where(tmdb_id: movie_attributes[:tmdb_id]).first_or_create do |movie|
+      movie.imdb_id = movie_attributes[:imdb_id]
+      movie.title = movie_attributes[:title]
+      movie.poster_path = movie_attributes[:poster_path]
+      movie.release_date = movie_attributes[:release_date]
+    end
+  end
+
+  def self.to_attributes(movie)
+    {
+      tmdb_id: movie.id,
+      imdb_id: movie.imdb_id,
+      title: movie.title,
+      poster_path: movie.poster_path,
+      release_date: movie.release_date
+    }
+  end
 end
